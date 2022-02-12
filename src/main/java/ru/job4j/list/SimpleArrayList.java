bc4ec0f2a4573c93b2af212c33dec2f722786209
g.burkov <g.burkov@cheb.eraga.net>
2022-02-12 22:09:07 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,79 @@
+package ru.job4j.list;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            this.container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[size] = value;
+        modCount++;
+        size++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        T oldValue = get(index);
+        container[index] = newValue;
+        modCount++;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        T oldValue = get(index);
+        System.arraycopy(container, index + 1, container, index, container.length - 1 - index);
+        container[container.length - 1] = null;
+        modCount++;
+        size--;
+        return oldValue;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            final int expectedModCount = modCount;
+            int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[cursor++];
+            }
+        };
+    }
+}
