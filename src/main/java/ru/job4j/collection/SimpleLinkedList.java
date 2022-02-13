1bfa316a1dcee52e6de653af63cd9d54318bcbb4
g.burkov <g.burkov@cheb.eraga.net>
2022-02-13 12:51:48 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,74 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int modCount;
+    transient int size = 0;
+    transient Node<E> fstNode;
+    transient Node<E> lstNode;
+
+    private static class Node<E> {
+        E currentElement;
+        Node<E> nextElement;
+        Node<E> prevElement;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.currentElement = element;
+            this.nextElement = next;
+            this.prevElement = prev;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        final Node<E> l = lstNode;
+        final Node<E> newNode = new Node<>(l, value, null);
+        lstNode = newNode;
+        if (l == null) {
+            fstNode = newNode;
+        } else {
+            l.nextElement = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> target = fstNode;
+        for (int i = 0; i < index; i++) {
+            target = target.nextElement;
+        }
+        return target.currentElement;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            final int expectedModCount = modCount;
+            int cursor = 0;
+            Node<E> next = fstNode;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> lastReturned = next;
+                next = next.nextElement;
+                cursor++;
+                return lastReturned.currentElement;
+            }
+        };
+    }
+}
