849f35c893d344cdf29b991e172eea4a8f3ca598
g.burkov <g.burkov@cheb.eraga.net>
2022-03-18 18:47:12 +0300

    3. Фильтры [#1732]

---
+++ src/main/resources/filters_products/insert.sql
@@ -0,0 +1,33 @@
+insert into type(name) values ('СЫР');
+insert into type(name) values ('МОЛОКО');
+insert into type(name) values ('МЯСО');
+insert into type(name) values ('ОВОЩИ');
+insert into type(name) values ('ФРУКТЫ');
+
+insert into product(name, type_id, expired_date, price) values ('Сыр плавленный', 1, '20.03.2022', 100.00);
+insert into product(name, type_id, expired_date, price) values ('Сыр моцарелла', 1, '19.03.2022', 150.00);
+insert into product(name, type_id, expired_date, price) values ('Сыр Хохланд', 1, '15.03.2022', 200.00);
+
+insert into product(name, type_id, expired_date, price) values ('Молоко ''Домик в деревне''', 2, '21.03.2022', 70.00);
+insert into product(name, type_id, expired_date, price) values ('Кефир ''Простоквашино''', 2, '20.03.2022', 80.00);
+insert into product(name, type_id, expired_date, price) values ('Творог ''Акконд''', 2, '16.03.2022', 90.00);
+insert into product(name, type_id, expired_date, price) values ('Мороженое ''Пломбир''', 2, '17.03.2022', 75.00);
+insert into product(name, type_id, expired_date, price) values ('Мороженое ''Шоколадное''', 2, '19.03.2022', 85.00);
+insert into product(name, type_id, expired_date, price) values ('Мороженое ''Ореховое''', 2, '22.03.2022', 95.00);
+insert into product(name, type_id, expired_date, price) values ('Мороженое ''Фисташковое''', 2, '26.03.2022', 105.00);
+insert into product(name, type_id, expired_date, price) values ('Ряженка', 2, '22.03.2022', 50.00);
+insert into product(name, type_id, expired_date, price) values ('Простокваша', 2, '23.03.2022', 55.00);
+insert into product(name, type_id, expired_date, price) values ('Снежок', 2, '24.03.2022', 60.00);
+insert into product(name, type_id, expired_date, price) values ('Бифидок', 2, '25.03.2022', 51.00);
+
+insert into product(name, type_id, expired_date, price) values ('Говядина', 3, '25.03.2022', 350.00);
+insert into product(name, type_id, expired_date, price) values ('Свинина', 3, '20.03.2022', 300.00);
+insert into product(name, type_id, expired_date, price) values ('Курица', 3, '17.03.2022', 250.00);
+
+insert into product(name, type_id, expired_date, price) values ('Картофель', 4, '20.03.2022', 50.00);
+insert into product(name, type_id, expired_date, price) values ('Морковь', 4, '22.03.2022', 40.00);
+insert into product(name, type_id, expired_date, price) values ('Лук', 4, '14.03.2022', 30.00);
+
+insert into product(name, type_id, expired_date, price) values ('Бананы', 5, '23.03.2022', 120.00);
+insert into product(name, type_id, expired_date, price) values ('Апельсины', 5, '20.03.2022', 130.00);
+insert into product(name, type_id, expired_date, price) values ('Яблоки', 5, '10.03.2022', 60.00);
---
+++ src/main/resources/filters_products/queries.sql
@@ -0,0 +1,32 @@
+select p.*
+from product p
+         join type t on p.type_id = t.id
+where t.name = 'СЫР';
+
+select *
+from product p
+where p.name like 'Мороженое%';
+
+select *
+from product p
+where p.expired_date < current_date;
+
+select p.*
+from product p
+where p.price = (select max(price) from product);
+
+select t.name as имя_типа, count(*) as Количество
+from type t
+         join product p on t.id = p.type_id
+group by t.name;
+
+select *
+from product p
+         join type t on p.type_id = t.id
+where t.name in ('СЫР', 'МОЛОКО');
+
+select t.name as имя_типа, count(*) as Количество
+from product p
+         join type t on p.type_id = t.id
+group by t.name
+having count(*) < 10;
